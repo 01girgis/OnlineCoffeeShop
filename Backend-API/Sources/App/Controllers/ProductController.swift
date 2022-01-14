7f222edf4a14f9e701131e4abbdc1af0cceb75ed
crus <romanghiet05@gmail.com>
2022-01-14 14:58:14 -0800

    coding product model controller

--- Backend-API/Sources/App/Controllers/ProductController.swift
+++ Backend-API/Sources/App/Controllers/ProductController.swift
@@ -3,3 +3,12 @@
 //
 //  Created by Romany GirGis, 2022.
 //  Copyright © 2022 All rights reserved
+
+import Vapor
+
+//Database Processes
+struct ProductController:RouteCollection{
+    func boot(routes: RoutesBuilder) throws {
+        let dataTable = routes.grouped("products")
+    }
+}
