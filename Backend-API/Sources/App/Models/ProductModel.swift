f2f7839933a3ac894138eb6d210d67dc46ec2c61
crus <romanghiet05@gmail.com>
2022-01-13 17:26:56 -0800

    class Model

--- Backend-API/Sources/App/Models/ProductModel.swift
+++ Backend-API/Sources/App/Models/ProductModel.swift
@@ -3,3 +3,14 @@
 //
 //  Created by Romany GirGis, 2021.
 //  Copyright © 2022 All rights reserved
+
+import FluentKit
+import Vapor
+
+
+//Base Model Class
+final class Product: Model,Content{
+    //schema select
+    let schema = "products"
+    
+}
