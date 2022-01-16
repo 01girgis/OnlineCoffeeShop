02c9a6e572cbeb2c363f0991eb60aece963944bc
crus <romanghiet05@gmail.com>
2022-01-15 17:19:03 -0800

    coding post request

--- Backend-API/Sources/App/Controllers/ProductController.swift
+++ Backend-API/Sources/App/Controllers/ProductController.swift
@@ -13,10 +13,19 @@ struct ProductController:RouteCollection{
         
         //Get Request
         dataTable.get(use: getMethod)
+        
+        //Post Request
+        dataTable.post(use: postMethod)
     }
     
     //Get Request Function
     func getMethod(req:Request) throws -> EventLoopFuture<[Product]>{
         return Product.query(on: req.db).all()
     }
+    
+    func postMethod(req:Request) throws -> EventLoopFuture<HTTPStatus>{
+        let sendPost = try req.content.decode(Product.self)
+        return sendPost.save(on: req.db).transform(to: .ok)
+    }
+    
 }
