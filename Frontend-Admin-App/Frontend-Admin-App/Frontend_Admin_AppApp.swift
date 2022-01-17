a9a858be37eb612cbb921ba357d304da73dbff28
crus <romanghiet05@gmail.com>
2022-01-16 16:57:39 -0800

    initial client side admin app

---
+++ Frontend-Admin-App/Frontend-Admin-App/Frontend_Admin_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frontend_Admin_AppApp.swift
+//  Frontend-Admin-App
+//
+//  Created by crus on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Frontend_Admin_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
