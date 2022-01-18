e27bec1d44d0da20b2374d6d55b619fa118c95bf
crus <romanghiet05@gmail.com>
2022-01-17 18:14:56 -0800

    build Model data structure

--- Frontend-Admin-App/Frontend-Admin-App/Model/Product.swift
+++ Frontend-Admin-App/Frontend-Admin-App/Model/Product.swift
@@ -5,3 +5,12 @@
 //  Copyright © 2022 All rights reserved
 
 import Foundation
+
+//Model Structure
+struct Product: Identifiable , Codable{
+    //Data fields
+    var id:UUID?
+    var name:String
+    var price:Float
+    var description:String?
+}
