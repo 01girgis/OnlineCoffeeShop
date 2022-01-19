0c4c1bb09e82305b9dba4afc89c76381220a66d5
crus <romanghiet05@gmail.com>
2022-01-18 20:19:44 -0800

    set API url parameters

---
+++ Frontend-Admin-App/Frontend-Admin-App/NetworkCore/ParamPoints.swift
@@ -0,0 +1,16 @@
+//
+//
+//
+//  Created by Romany GirGis, 2022.
+//  Copyright © 2022 All rights reserved
+
+import Foundation
+
+//enumeration of communication url
+enum FirstP{
+    static let url = ""
+}
+
+enum EndP{
+    static let point = "/"
+}
