60cc229ee079a11d9f7bbb5af1f3e572a8911c8b
crus <romanghiet05@gmail.com>
2022-01-18 20:04:58 -0800

    check  http get response

--- Frontend-Admin-App/Frontend-Admin-App/NetworkCore/HttpSniffer.swift
+++ Frontend-Admin-App/Frontend-Admin-App/NetworkCore/HttpSniffer.swift
@@ -9,11 +9,22 @@ import Foundation
 
 //API communication error
 enum ApiErr:Error {
-    case  invalidRsponse , badUrl
+    case  invalidRsponse , badUrl , noDecoding
 }
 
 // basic services class
 class HttpSniffer {
     
+    //Http Get Request
+    func FetchData<T:Codable> (url:URL) async throws -> [T] {
+        
+        //start Data exchange session
+        let (data,response) = try await URLSession.shared.data(from: url)
+        
+        //Check Http Response status code
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw ApiErr.invalidRsponse
+        }
+    }
     
 }
