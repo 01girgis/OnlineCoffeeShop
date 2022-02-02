4f9444fbe0254972c5af449f06008e9d75564917
crus <romanghiet05@gmail.com>
2022-02-02 04:41:34 -0800

    build sub ViewModel

---
+++ Frontend-Admin-App/Frontend-Admin-App/ViewModel/SubViewModel/ActionSheetViewModel.swift
@@ -0,0 +1,18 @@
+//
+//
+//
+//  Created by Romany GirGis, 2022.
+//  Copyright © 2022 All rights reserved
+
+import Foundation
+
+//Sub View class
+final class ActionSheetViewModel:ObservableObject{
+    //Add Main Model fields
+    @Published var myID:UUID?
+    @Published var name:String = ""
+    @Published var price:String  = ""
+    @Published var description:String = ""
+    
+    
+}
--- Frontend-Admin-App/Frontend-Admin-App/ViewModel/SubViewModel/SheetModalViewModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//
-//
-//  Created by Romany GirGis, 2022.
-//  Copyright © 2022 All rights reserved
-
-import Foundation
